2bf30aa03494594b8cbd04d10a7923d2e88a4dbd
unknown <luxassilva@gmail.com>
2023-02-03 12:38:31 -0300

    .

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 
 const connect = async () => {
     try {
+        mongoose.set('strictQuery', false);
         await mongoose.connect(
             "mongodb+srv://mrain00:Lacaverna1@cluster0.ghzsz.mongodb.net/ArgentinaPrograma?retryWrites=true&w=majority",
             {
